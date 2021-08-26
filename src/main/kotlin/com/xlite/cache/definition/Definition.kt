0c63d0cab06fb0c420dbb8ea1d0c32889d11874e
Tyler Telis <xlitersps@gmail.com>
2021-08-25 21:09:51 -0400

    Removing files from git

--- src/main/kotlin/com/xlite/cache/definition/Definition.kt
+++
@@ -1,9 +0,0 @@
-package com.xlite.cache.definition
-
-/**
- * @author Tyler Telis
- * @email <xlitersps@gmail.com>
- */
-internal interface Definition<T> {
-    fun decode(id: Int, data: ByteArray): T
-}
--- src/main/kotlin/com/xlite/cache/definition/StructType.kt
+++
@@ -1,3 +0,0 @@
-package com.xlite.cache.definition
-
-data class StructType(var id: Int = 0, var params: HashMap<Long, Any> = hashMapOf())
