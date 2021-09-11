78cf4c62be4180ceaa7c556503737f47e638a310
Tyler Telis <xlitersps@gmail.com>
2021-09-11 19:19:09 -0400

    Loc type

--- loader/src/main/kotlin/com/xlite/loader/Loader.kt
+++ loader/src/main/kotlin/com/xlite/loader/Loader.kt
@@ -2,6 +2,7 @@ package com.xlite.loader
 
 import com.xlite.loader.group.config.spotanim.SpotAnimationEntryProvider
 import com.xlite.loader.group.config.struct.StructEntryProvider
+import com.xlite.loader.group.loc.LocEntryProvider
 import com.xlite.loader.group.map.MapEntryProvider
 import com.xlite.loader.group.map.MapLocationEntryProvider
 import com.xlite.loader.group.obj.ObjEntryProvider
@@ -14,9 +15,11 @@ private val structEntryProvider = StructEntryProvider()
 private val mapEntryProvider = MapEntryProvider()
 private val objProvider = ObjEntryProvider()
 private val mapLocationEntryProvider = MapLocationEntryProvider()
+private val locEntryProvider = LocEntryProvider()
 
 fun spotAnimations(): SpotAnimationEntryProvider = spotAnimationEntryProvider
 fun structs(): StructEntryProvider = structEntryProvider
 fun maps(): MapEntryProvider = mapEntryProvider
 fun objs(): ObjEntryProvider = objProvider
 fun mapLocs(): MapLocationEntryProvider = mapLocationEntryProvider
+fun locs(): LocEntryProvider = locEntryProvider
