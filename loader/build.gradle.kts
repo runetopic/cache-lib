2148929ff159d1531bcd401314e6ad77980d9c4b
Tyler Telis <xlitersps@gmail.com>
2021-09-12 19:25:39 -0400

    Setup project for publishing to maven central

--- loader/build.gradle.kts
+++ loader/build.gradle.kts
@@ -1,5 +1,38 @@
 plugins {
     kotlin("jvm")
+    `maven-publish`
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            pom {
+                name.set("Xlite Cache Library Type Loaders")
+                description.set("Definition types of runescape files.")
+                url.set("https://github.com/xlite2/cache-lib")
+
+                developers {
+                    developer {
+                        id.set("tylert")
+                        name.set("Tyler Telis")
+                        email.set("xlitersps@gmail.com")
+                    }
+
+                    developer {
+                        id.set("ultraviolet-jordan")
+                        name.set("Jordan Abraham")
+                    }
+                }
+            }
+        }
+        create<MavenPublication>("maven") {
+            groupId = "com.xlite.cache"
+            artifactId = "loader"
+            version = "647.0-SNAPSHOT"
+
+            from(components["java"])
+        }
+    }
 }
 
 dependencies {
